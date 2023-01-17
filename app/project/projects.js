6a44a3559075031b75030ea4a5fba41ad0753ebb
4m4Sec <52249849+4m4Sec@users.noreply.github.com>
2023-01-17 23:35:22 +0100

    Add files via upload

--- app/project/projects.js
+++ app/project/projects.js
@@ -1,19 +1,34 @@
 function getProjects(){
     return {
-        "ARS-SHELL-CRYPT": {
-            getDescription: function (){ return "ARS-SHELL-CRYPT is a modified caesar's-cipher-based encrypt system written in C++."; },
-            getLink: function(){ return "https://github.com/4m4Sec/ARS_SHELL_CRYPT"; },
-            getSatetement: function (){ return "Completed"; }
+        "Websites": {
+            "Lets-Freerun": {
+                getDescription: function (){ return "Lets-Freerun is a website which gather a lot of parkour spots around the world."; },
+                getLink: function(){ return "https://github.com/4m4Sec/Lets-Freerun"; },
+                getSatetement: function (){ return "Under Development"; },
+                getIcon: function (){ return "human"; }
+            }
         },
-        "Lets-Freerun": {
-            getDescription: function (){ return "Lets-Freerun is a website which gather a lot of parkour spots around the world."; },
-            getLink: function(){ return "https://github.com/4m4Sec/Lets-Freerun"; },
-            getSatetement: function (){ return "Under Development"; }
+        "Tools": {
+            "Kharon": {
+                getDescription: function (){ return "Kharon is an automated web-server ctf basic scan which perform basic tasks of webserver pentesting. Written in Python."; },
+                getLink: function(){ return "https://github.com/4m4Sec/Kharon"; },
+                getSatetement: function (){ return "Under Development"; },
+                getIcon: function (){ return ""; }
+            },
+            "Theia": {
+                getDescription: function (){ return "Theia is an ip-lookup OSINT tool for linux written in Python2."; },
+                getLink: function(){ return "https://github.com/4m4Sec/Theia"; },
+                getSatetement: function (){ return "Completed"; },
+                getIcon: function (){ return ""; }
+            },
         },
-        "Kharon": {
-            getDescription: function (){ return "Lets-Freerun is a website which gather a lot of parkour spots around the world."; },
-            getLink: function(){ return "https://github.com/4m4Sec/Kharon"; },
-            getSatetement: function (){ return "Under Development"; }
-        },
+        "Others": {
+            "ARS-SHELL-CRYPT": {
+                getDescription: function (){ return "ARS-SHELL-CRYPT is a modified caesar's-cipher-based encrypt system written in C++."; },
+                getLink: function(){ return "https://github.com/4m4Sec/ARS_SHELL_CRYPT"; },
+                getSatetement: function (){ return "Completed"; },
+                getIcon: function (){ return "lock"; }
+            },
+        }
     }
 }
