a0973e951568ed0aba65fa2c66d9afdb47523756
4m4Sec <52249849+4m4Sec@users.noreply.github.com>
2023-01-18 19:51:01 +0100

    Add files via upload

--- app/project/projects.js
+++ app/project/projects.js
@@ -2,7 +2,7 @@ function getProjects(){
     return {
         "Websites": {
             "Lets-Freerun": {
-                getDescription: function (){ return "Lets-Freerun is a website which gather a lot of parkour spots around the world."; },
+                getDescription: function (){ return "website which gather a lot of parkour spots around the world."; },
                 getLink: function(){ return "https://github.com/4m4Sec/Lets-Freerun"; },
                 getSatetement: function (){ return "Under Development"; },
                 getIcon: function (){ return "human"; }
@@ -10,21 +10,21 @@ function getProjects(){
         },
         "Tools": {
             "Kharon": {
-                getDescription: function (){ return "Kharon is an automated web-server ctf basic scan which perform basic tasks of webserver pentesting. Written in Python."; },
+                getDescription: function (){ return "automated ctf web-server scan which perform basic tasks of webserver pentesting."; },
                 getLink: function(){ return "https://github.com/4m4Sec/Kharon"; },
                 getSatetement: function (){ return "Under Development"; },
-                getIcon: function (){ return ""; }
+                getIcon: function (){ return "skull"; }
             },
             "Theia": {
-                getDescription: function (){ return "Theia is an ip-lookup OSINT tool for linux written in Python2."; },
+                getDescription: function (){ return "ip-lookup OSINT tool for linux."; },
                 getLink: function(){ return "https://github.com/4m4Sec/Theia"; },
                 getSatetement: function (){ return "Completed"; },
-                getIcon: function (){ return ""; }
+                getIcon: function (){ return "search-web"; }
             },
         },
         "Others": {
             "ARS-SHELL-CRYPT": {
-                getDescription: function (){ return "ARS-SHELL-CRYPT is a modified caesar's-cipher-based encrypt system written in C++."; },
+                getDescription: function (){ return "modified caesar's-cipher-based encrypt system written in C++."; },
                 getLink: function(){ return "https://github.com/4m4Sec/ARS_SHELL_CRYPT"; },
                 getSatetement: function (){ return "Completed"; },
                 getIcon: function (){ return "lock"; }
