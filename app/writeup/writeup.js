e717c57b1fcc6cc8ba2cd3e67e9ba7ec96202bc1
4m4Sec <52249849+4m4Sec@users.noreply.github.com>
2023-06-04 20:53:41 +0200

    update [2.1.0] : start 'wonderland' writeup

--- app/writeup/writeup.js
+++ app/writeup/writeup.js
@@ -1,22 +1,32 @@
 function getWriteups(){
     return {
-        "bounty-hacker": {
-            getName: function (){ return "Bounty Hacker"; },
+        "wonderland": {
+            getName: function (){ return "Wonderland"; },
             getLocation: function() { return "TryHackMe"; },
-            getLink: function(){ return "thm/bounty-hacker/"; },
-            getDifficulty: function (){ return "Easy" }
-        },
-        "simple-ctf": {
-            getName: function (){ return "Simple CTF"; },
-            getLocation: function() { return "TryHackMe"; },
-            getLink: function(){ return "thm/simple-ctf/"; },
-            getDifficulty: function (){ return "Easy" }
+            getLink: function(){ return "thm/wonderland/"; },
+            getDifficulty: function (){ return "Medium" },
+            getDate: function (){ return "06/04/2023" }
         },
         "ignite": {
             getName: function (){ return "Ignite"; },
             getLocation: function() { return "TryHackMe"; },
             getLink: function(){ return "thm/ignite/"; },
-            getDifficulty: function (){ return "Easy" }
+            getDifficulty: function (){ return "Easy" },
+            getDate: function (){ return "06/03/2023" }
+        },
+        "simple-ctf": {
+            getName: function (){ return "Simple CTF"; },
+            getLocation: function() { return "TryHackMe"; },
+            getLink: function(){ return "thm/simple-ctf/"; },
+            getDifficulty: function (){ return "Easy" },
+            getDate: function (){ return "06/02/2023" }
+        },
+        "bounty-hacker": {
+            getName: function (){ return "Bounty Hacker"; },
+            getLocation: function() { return "TryHackMe"; },
+            getLink: function(){ return "thm/bounty-hacker/"; },
+            getDifficulty: function (){ return "Easy" },
+            getDate: function (){ return "06/01/2023" }
         }
     }
 }
