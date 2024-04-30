48fb9a900e5a0924b7b55d9f77c2336d39687374
4m4Sec <neptune.dev.cybsec@gmail.com>
2024-04-30 10:04:37 +0200

    update [3.2.7] : add thumbnails for writeups

--- app/writeup/writeup.js
+++ app/writeup/writeup.js
@@ -1,46 +1,60 @@
 function getWriteups(){
     return {
+        "headless": {
+            getName: function (){ return "Headless"; },
+            getLocation: function (){ return "HackTheBox"; },
+            getLink: function (){ return "htb/headless/"; },
+            getDifficulty: function (){ return "Easy"; },
+            getDate: function (){ return "04/30/2024" },
+            getThumb: function (){ return "https://labs.hackthebox.com/storage/avatars/26e076db204a74b99390e586d7ebcf8c.png"; }
+        },
         "cmess": {
             getName: function(){ return "CMesS"; },
             getLocation: function(){ return "TryHackMe"; },
             getLink: function(){ return "thm/cmess/"; },
             getDifficulty: function(){ return "Medium"; },
-            getDate: function(){ return "07/13/2023"; }
+            getDate: function(){ return "07/13/2023"; },
+            getThumb: function (){ return "https://tryhackme-images.s3.amazonaws.com/room-icons/1516b0c85bb9f7312a88638df5b0f3af.png"; }
         },
         "looking-glass": {
             getName: function(){ return "Looking Glass"; },
             getLocation: function(){ return "TryHackMe"; },
             getLink: function(){ return "thm/looking-glass/"; },
             getDifficulty: function(){ return "Medium"; },
-            getDate: function(){ return "06/11/2023"; }
+            getDate: function(){ return "06/11/2023"; },
+            getThumb: function (){ return "https://tryhackme-images.s3.amazonaws.com/room-icons/56215a135c08963843afda2240c317a3.png"; }
         },
         "wonderland": {
             getName: function (){ return "Wonderland"; },
             getLocation: function() { return "TryHackMe"; },
             getLink: function(){ return "thm/wonderland/"; },
             getDifficulty: function (){ return "Medium" },
-            getDate: function (){ return "06/04/2023" }
+            getDate: function (){ return "06/04/2023" },
+            getThumb: function (){ return "https://tryhackme-images.s3.amazonaws.com/room-icons/fdba6eaf85513262b2a9b12875b0f342.jpeg"; }
         },
         "ignite": {
             getName: function (){ return "Ignite"; },
             getLocation: function() { return "TryHackMe"; },
             getLink: function(){ return "thm/ignite/"; },
             getDifficulty: function (){ return "Easy" },
-            getDate: function (){ return "06/03/2023" }
+            getDate: function (){ return "06/03/2023" },
+            getThumb: function (){ return "https://tryhackme-images.s3.amazonaws.com/room-icons/676cb3273c613c9ba00688162efc0979.png"; }
         },
         "simple-ctf": {
             getName: function (){ return "Simple CTF"; },
             getLocation: function() { return "TryHackMe"; },
             getLink: function(){ return "thm/simple-ctf/"; },
             getDifficulty: function (){ return "Easy" },
-            getDate: function (){ return "06/02/2023" }
+            getDate: function (){ return "06/02/2023" },
+            getThumb: function (){ return "https://tryhackme-images.s3.amazonaws.com/room-icons/f28ade2b51eb7aeeac91002d41f29c47.png"; }
         },
         "bounty-hacker": {
             getName: function (){ return "Bounty Hacker"; },
             getLocation: function() { return "TryHackMe"; },
             getLink: function(){ return "thm/bounty-hacker/"; },
             getDifficulty: function (){ return "Easy" },
-            getDate: function (){ return "06/01/2023" }
+            getDate: function (){ return "06/01/2023" },
+            getThumb: function (){ return "https://tryhackme-images.s3.amazonaws.com/room-icons/9ad38a2cc31d6ae0030c888aca7fe646.jpeg"; }
         }
     }
 }
